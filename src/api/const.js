6ed701296d4570ab6e86fca46f65206a9347144d
Natalia Homusko <avgust_net85@mail.ru>
2023-04-21 14:35:32 +0300

    change URL

--- src/api/const.js
+++ src/api/const.js
@@ -4,7 +4,7 @@ export const ACCESS_KEY = 'tay6JT3FKSTVgZOMnR81Dz2MW2nbD1A7IFyb3Gg3i3c';
 export const SECRET_KEY = 'glPjKTX4_J8NI5LozeQV2tlfEA8BuWDhYX9OFG2STH4';
 // авторизация пользователя
 export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize?';
-export const REDIRECT_URI = 'http://localhost:3000/';
+export const REDIRECT_URI = 'https://gallery-pink.vercel.app/auth';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_photos read_user write_likes';
 export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
